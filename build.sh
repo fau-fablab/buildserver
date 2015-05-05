6e75cf4fb66dfa59a5982866e6e8d3c2e162f680
sedrubal <sebastian.endres@online.de>
2015-05-06 01:49:19 +0200

    added server-side todo counter, also fixes #2

--- build.sh
+++ build.sh
@@ -42,20 +42,24 @@ function build-with-submodule() {
 	git reset --hard origin > /dev/null
 	git submodule update --init > /dev/null
 	commit_id=$(git log --pretty=format:"%h" --abbrev-commit --date=short -1)
+	todos=0
+	while IFS= read -r -d '' f; do   todos=$(($todos+$(grep -wc -i "todo" "$f" &2> /dev/null))); done < <(find . -maxdepth 1 -type f -name "*.tex" -print0)
+	while IFS= read -r -d '' f; do   todos=$(($todos+$(grep -wc -i "todo" "$f" &2> /dev/null))); done < <(find . -maxdepth 1 -type f -name "*.md" -print0)
 	make > /dev/null
 	mkdir -p $OUTPUT_DIR
 	cd output
 	cp ./*.* $OUTPUT_DIR
-	update-status $1 success $commit_id
+	update-status $1 success
 	popd  > /dev/null
 	commit_id=""
+	todos=""
 }
 
 # If the exit value of the script > 0 then the current $repo build seems to be failed (set -e causes this)
 function handle-exit() {
 	if (( $? > 0 )) ; then
         	echo "[!] Build repository '${repo}' failed!" 1>&2
-		update-status $repo failed $commit_id
+		update-status $repo failed
 		current_repo_index=$((current_repo_index+1))
 		# run this script recursively, but start with next index
 		$0 $current_repo_index
@@ -73,13 +77,28 @@ function update-status() {
 	mkdir -p "$HOME/public_html/$1"
 	STATUS_OUT_DIR="$HOME/public_html/$1/"
 	STATUS_DIR="$HOME/status-icons/"
+	TODOS_SVG_TEMPLATE="${STATUS_DIR}todos.svg.template"
 	cp "${STATUS_DIR}build-${2}.svg" "${STATUS_OUT_DIR}status.svg"
 	if [[ -z "$commit_id" || "$commit_id" == "" ]]; then
 	    commit_info=""
 	else
 	    commit_info=", \"commit\": \"${commit_id}\""
 	fi
-	echo "{ \"status\": \"${2}\", \"updated\": \"$(date +%s)\", \"updated-human\": \"$(date)\"${commit_info} }" > "${STATUS_OUT_DIR}status.json"
+	if [[ -z "$todos" || "$todos" == "" ]]; then
+	    todos_info=""
+	else
+	    todos_info=", \"todos\": \"${todos}\""
+	    fg_color=("#323232" "#323232" "#646464")
+	    bg_color=("#149123" "#b6ba19" "#911414")
+	    index=0
+	    if (( $todos > 5 )) ; then
+	        index=1
+	    elif (( $todos > 20 )) ; then
+	        index=2
+	    fi
+	    sed "s/\$todos/$todos/g" $TODOS_SVG_TEMPLATE | sed "s/\$fg_color/${fg_color[${index}]}/g" - | sed "s/\$bg_color/${bg_color[${index}]}/g" - > "${STATUS_OUT_DIR}status-todos.svg"
+	fi
+	echo "{ \"status\": \"${2}\", \"updated\": \"$(date +%s)\", \"updated-human\": \"$(date)\"${commit_info}${todos_info} }" > "${STATUS_OUT_DIR}status.json"
 }
 
 # usage: run <start_index>
